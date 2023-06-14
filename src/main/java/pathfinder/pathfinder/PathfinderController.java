869cff1e4ee13cb681a29662122bd0c232dbdc83
oliver <oliver.pecek@gmail.com>
2023-06-14 20:15:38 +0200

    added checkerboard, it is misaligned for some reason

--- src/main/java/pathfinder/pathfinder/PathfinderController.java
+++ src/main/java/pathfinder/pathfinder/PathfinderController.java
@@ -2,12 +2,18 @@ package pathfinder.pathfinder;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
-public class PathfinderController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PathfinderController implements Initializable {
 
   @FXML
   private RadioButton blockRadioButton;
@@ -27,9 +33,44 @@ public class PathfinderController {
   @FXML
   private RadioButton startRadioButton;
 
+  private int gridRows;
+  private int gridCols;
+
   @FXML
   void start(ActionEvent event) {
+    System.out.println("start button works");
+  }
+
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+    gridCols = 8;
+    gridRows = 8;
+    setUpGrid(grid, 600, 600, gridRows, gridCols);
+    grid.setOnMouseClicked(mouseEvent -> {
+      Rectangle rect = (Rectangle) mouseEvent.getTarget();
+      Color color = (Color) rect.getFill();
+      if (color == Color.RED) {
+        rect.setFill((Color) rect.getUserData());
+      } else {
+        rect.setFill(Color.RED);
+      }
+    });
 
   }
 
+  public void setUpGrid(GridPane gp, int length, int height, int row, int column) {
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < column; j++) {
+        Rectangle rectangle = new Rectangle(((double) length / row), ((double) height / column));
+        if ((i + j) % 2 == 0) {
+          rectangle.setFill(Color.WHITE);
+          rectangle.setUserData(Color.WHITE);
+        } else {
+          rectangle.setFill(Color.GRAY);
+          rectangle.setUserData(Color.GRAY);
+        }
+        gp.add(rectangle, i, j);
+      }
+    }
+  }
 }
